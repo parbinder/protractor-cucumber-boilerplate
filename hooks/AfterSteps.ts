462a4d691a5763a79a02807026ac515bb621f462
Parbinder Singh <parbinder.qa@gmail.com>
2022-11-18 14:45:01 +0530

    Added support of add screenshot to each step

--- hooks/AfterSteps.ts
+++ hooks/AfterSteps.ts
@@ -4,8 +4,8 @@ import { browser } from 'protractor';
 /**
  * Runs after each step
  */
-AfterStep(async function (scenario) {
-    if (scenario.result.status === Status.FAILED) {
+AfterStep(async function (scenario) {    
+    if (scenario.result.status === Status.FAILED || Number(process.env['DEBUG']) == 1) {
         // tslint:disable-next-line: no-invalid-this
         this.attach(await browser.takeScreenshot(), 'image/png');
     }
